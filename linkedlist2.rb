874f590b9be6296469c54229a72db9220de3aeaa
shhuang123 <sharonhuang923@gmail.com>
2019-05-31 11:06:38 -0700

    linkedlist

---
+++ linkedlist2.rb
@@ -0,0 +1,37 @@
+class LinkedListNode
+  attr_accessor :value, :next_node
+
+  def initialize(value, next_node=nil)
+    @value = value
+    @next_node = next_node
+  end
+end
+
+  def reverse_list(list, previous=nil)
+    a_node = list.next_node
+    list.next_node = previous
+    if a_node
+      reverse_list(a_node, list)
+    else
+      list
+    end
+  end
+
+  def print_values(list_node)
+    if list_node
+      print "#{list_node.value} --> "
+      print_values(list_node.next_node)
+    else
+      print "nil\n"
+      return
+    end
+  end
+
+node1 = LinkedListNode.new(37)
+node2 = LinkedListNode.new(99, node1)
+node3 = LinkedListNode.new(12, node2)
+
+print_values(node3)
+puts "-------"
+revlist = reverse_list(node3)
+print_values(revlist)
